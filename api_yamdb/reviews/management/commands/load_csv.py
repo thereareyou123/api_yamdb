50458e11e2c033997e7c133c2ee6dcd14dc5905e
Никита Рохлин <thereareyou@yahoo.com>
2024-07-02 22:04:32 +0300

    fix_load_csv

--- api_yamdb/reviews/management/commands/load_csv.py
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -1,45 +1,99 @@
-import csv
+from csv import DictReader
 
-from django.core.management.base import BaseCommand, CommandError
-from reviews.models import (
-    Category, Genre, Title, TitleGenre, Review, Comment
-)
+from django.core.management import BaseCommand
+from django.db.utils import IntegrityError
+
+from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
 from users.models import User
-
-from api_yamdb.settings import BASE_DIR
 
 
 class Command(BaseCommand):
     help = 'Command to automatically populate the database'
 
+    def load_users(self):
+        for row in DictReader(
+                open('static/data/users.csv', encoding='UTF-8')):
+            User.objects.get_or_create(**row)
+
+        self.stdout.write(self.style.SUCCESS(
+            'users.csv выгружен в бд.')
+        )
+
+    def load_category(self):
+        for row in DictReader(
+                open('static/data/category.csv', encoding='UTF-8')):
+            Category.objects.get_or_create(**row)
+
+        self.stdout.write(self.style.SUCCESS(
+            'category.csv выгружен в бд.')
+        )
+
+    def load_genre(self):
+        for row in DictReader(
+                open('static/data/genre.csv', encoding='UTF-8')):
+            Genre.objects.get_or_create(**row)
+
+        self.stdout.write(self.style.SUCCESS(
+            'genre.csv выгружен в бд.')
+        )
+
+    def load_title(self):
+        for row in DictReader(
+                open('static/data/titles.csv', encoding='UTF-8')):
+            Title.objects.get_or_create(
+                category=Category.objects.get(id=row.pop('category')), **row)
+
+        self.stdout.write(self.style.SUCCESS(
+            'titles.csv выгружен в бд.')
+        )
+
+    def load_genre_title(self):
+        for row in DictReader(
+                open('static/data/genre_title.csv')):
+            TitleGenre.objects.get_or_create(
+                title=Title.objects.get(id=row.pop('title_id')),
+                genre=Genre.objects.get(id=row.pop('genre_id')))
+
+        self.stdout.write(self.style.SUCCESS(
+            'genre_title.csv выгружен в бд.')
+        )
+
+    def load_review(self):
+        for row in DictReader(
+                open('static/data/review.csv', encoding='UTF-8')):
+            Review.objects.get_or_create(
+                title=Title.objects.get(pk=row.pop('title_id')),
+                author=User.objects.get(id=row.pop('author')),
+                **row)
+
+        self.stdout.write(self.style.SUCCESS(
+            'review.csv выгружен в бд.')
+        )
+
+    def load_comments(self):
+        for row in DictReader(
+                open('static/data/comments.csv', encoding='UTF-8')):
+            Comment.objects.get_or_create(
+                review=Review.objects.get(id=row.pop('review_id')),
+                author=User.objects.get(id=row.pop('author')),
+                **row)
+
+        self.stdout.write(self.style.SUCCESS(
+            'comments.csv выгружен в бд.')
+        )
+
     def handle(self, *args, **options):
+        try:
+            self.load_category()
+            self.load_genre()
+            self.load_title()
+            self.load_genre_title()
+            self.load_users()
+            self.load_review()
+            self.load_comments()
 
-        TABLES = {
-            Category: 'category.csv',
-            Comment: 'comments.csv',
-            Genre: 'genre.csv',
-            Review: 'review.csv',
-            Title: 'titles.csv',
-            TitleGenre: 'genre_title.csv',
-            User: 'users.csv',
-        }
-
-        for model, csv_f in TABLES.items():
-            with open(f'{BASE_DIR}/static/data/{csv_f}',
-                      'r', encoding='utf8') as file:
-                cvs_rows = csv.DictReader(file, delimiter=',')
-                for row in cvs_rows:
-                    shallow_copy = row.copy()
-                    for keys in shallow_copy.keys():
-                        if 'category' in keys:
-                            row['category_id'] = row.pop('category')
-                        elif 'author' in keys:
-                            row['author_id'] = row.pop('author')
-                    try:
-                        model.objects.create(**row)
-                    except ValueError as e:
-                        raise CommandError(
-                            f'Ошибка: {e}, файл {csv_f}, строка {row}'
-                        )
-            self.stdout.write(
-                f'Данные из таблицы {model.__name__} импортированы!')
+        except IntegrityError as err:
+            self.stdout.write(self.style.ERROR(
+                f'ERROR - {err}')
+            )
+            exit()
--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
         'username', 'email', 'role', 'first_name', 'last_name', 'bio'
     )
